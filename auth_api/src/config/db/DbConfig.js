985931a5db0980ea8d4fc2508241298c8b1f4ed2
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-26 13:40:04 -0300

    Sequelize

---
+++ auth_api/src/config/db/DbConfig.js
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize/dist";
+
+const sequelize = new Sequelize(
+    "auth-db", "root", "123456",{
+        host: "localhost",
+        dialect: "postgres",
+        quoteIdentifiers: false,
+        define:{
+            syncOnAssociation: true,
+            timestamp: false,
+            underscore: true,
+            underscoreAll: true,
+            freezeTableName: true
+        },
+    },
+);
+
+sequelize
+.authenticate()
+.then(() => {
+    console.log("Connection has been stablished! ")
+})
+.catch(err => {
+    console.error("Unable connect to the database")
+    console.error(err.message)
+});
+export default sequelize;
