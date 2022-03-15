7d8586ef3ddb0c67c835db2aaae479d83ee4a6a8
LuccasLpn <Luccas.lpn@outlook.com>
2022-03-14 21:56:43 -0300

    Created Find

--- product_api/src/main/java/academy/modules/supplier/repository/SupplierRepository.java
+++ product_api/src/main/java/academy/modules/supplier/repository/SupplierRepository.java
@@ -3,5 +3,9 @@ package academy.modules.supplier.repository;
 import academy.modules.supplier.domain.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+    
+    List<Supplier> findByNameIgnoreCaseContaining(String name);
 }
